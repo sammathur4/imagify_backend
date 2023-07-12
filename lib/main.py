bcc66a5124e558986f6dee7e98b586e8840122c2
muiz6 <muizhassan83@gmail.com>
2023-07-12 20:21:57 +0500

    remove bg endpoint is now working

--- lib/main.py
+++ lib/main.py
@@ -24,56 +24,59 @@ def aftert_request(response):
 
 @app.route("/remove_background", methods=["POST"])
 def remove_background():
-    # Get the uploaded images
-    files = request.files.getlist("image")
-    original_images = []
-    new_images = []
+    try:
+        # Get the uploaded images
+        files = request.files.getlist("image")
+        original_images = []
+        new_images = []
 
-    # Create folders if they don't exist
-    if not os.path.exists("user_images/original_images"):
-        os.makedirs("user_images/original_images")
-    if not os.path.exists("user_images/new_images"):
-        os.makedirs("user_images/new_images")
+        # Create folders if they don't exist
+        os.makedirs("user_images/original_images", exist_ok=True)
+        os.makedirs("user_images/new_images", exist_ok=True)
 
-    # Process each image using rembg
-    for i, file in enumerate(files):
-        img = Image.open(file)
-        output = remove(np.array(img.convert("RGB")))
+        # Process each image using rembg
+        for i, file in enumerate(files):
+            img = Image.open(file)
+            output = remove(np.array(img.convert("RGB")))
 
-        # Save original image locally
-        original_img_path = f"user_images/original_images/original_image_{i}.jpg"
-        img.convert("RGB").save(original_img_path)
+            # Save original image locally
+            original_img_path = f"user_images/original_images/original_image_{i}.jpg"
+            img.convert("RGB").save(original_img_path)
+            original_images.append(original_img_path)
 
-        # Save output image locally
-        new_img_path = f"user_images/new_images/new_image_{i}.png"
-        Image.fromarray(output).save(new_img_path)
+            # Save output image locally
+            new_img_path = f"user_images/new_images/new_image_{i}.png"
+            Image.fromarray(output).save(new_img_path)
+            new_images.append(new_img_path)
 
-        original_images.append(original_img_path)
-        new_images.append(new_img_path)
+        if len(new_images) == 1:
+            # If only one image was uploaded, return the processed image directly
+            output_img_path = new_images[0]
+            with open(output_img_path, "rb") as f:
+                response_bytes = f.read()
 
-    # If only one image was uploaded, return the processed image directly
-    if len(new_images) == 1:
-        output_img_path = new_images[0]
-        return send_file(
-            output_img_path,
-            mimetype="image/png",
-            as_attachment=True,
-            attachment_filename="processed_image.png",
-        )
+            response = make_response(response_bytes)
+            response.headers.set("Content-Type", "image/png")
+            response.headers.set(
+                "Content-Disposition", "attachment", filename="processed_image.png"
+            )
+            return response
 
-    # Otherwise, create a zip file containing all images
-    zip_filename = "processed_images.zip"
-    with zipfile.ZipFile(zip_filename, "w", zipfile.ZIP_DEFLATED) as zip_file:
-        for new_image_path in new_images:
-            zip_file.write(new_image_path, os.path.basename(new_image_path))
+        zip_filename = "processed_images.zip"
+        with zipfile.ZipFile(zip_filename, "w", zipfile.ZIP_DEFLATED) as zip_file:
+            for new_image_path in new_images:
+                zip_file.write(new_image_path, os.path.basename(new_image_path))
 
-    with open(zip_filename, "rb") as f:
-        response_bytes = f.read()
+        with open(zip_filename, "rb") as f:
+            response_bytes = f.read()
 
-    response = make_response(response_bytes)
-    response.headers.set("Content-Type", "application/zip")
-    response.headers.set("Content-Disposition", "attachment", filename=zip_filename)
-    return response
+        response = make_response(response_bytes)
+        response.headers.set("Content-Type", "application/zip")
+        response.headers.set("Content-Disposition", "attachment", filename=zip_filename)
+        return response
+
+    except Exception as e:
+        return str(e), 500
 
 
 @app.route("/add_background", methods=["POST"])
